16e8f61d9d3c1e7f7fd2d16d3951f6003e26948f
Vlad-Russ <70793269+Vlad-Russ@users.noreply.github.com>
2021-02-23 19:39:38 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
 import math
 import json
 
-
-x = 10
-a = (x ** 7 + 22 * x ** 2)
-b = (38 * x ** 5 - 26 * x ** 6) / (math.tan(x) - math.log10(x))
-c = ((x ** 6) / 85) - 2 * x ** 3 - 56
-resultat = a + math.sqrt(b) - c
-print(f"{resultat:.2e}")
+#
+# x = 10
+# a = (x ** 7 + 22 * x ** 2)
+# b = (38 * x ** 5 - 26 * x ** 6) / (math.tan(x) - math.log10(x))
+# c = ((x ** 6) / 85) - 2 * x ** 3 - 56
+# resultat = a + math.sqrt(b) - c
+# print(f"{resultat:.2e}")
 
 a = [[10, 260, 4, 64, 3010, 2], [10, 200, 4, 128, 4030, 3], [8, 200, 6, 64, 4160, 2]]
 n = 0
